7d2acd5bb761617546646512758e77525bbd8840
snehagautam5151 <interio.sneha@gmail.com>
2020-07-27 18:39:15 +0530

    thorsten project done

---
+++ Javashortproject/src/Databs.java
@@ -0,0 +1,11 @@
+
+	public class Databs
+	   {
+		public static void main(String[] args)
+		{
+			String firstName;
+			firstName = "Thorsten is great!";
+			System.out.println(firstName);
+		}
+	   }
+
