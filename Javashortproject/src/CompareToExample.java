b8d9e1c2d1bf597d72b03eefee702a249fe75140
snehagautam5151 <66638327+snehagautam5151@users.noreply.github.com>
2020-07-29 17:42:07 +0530

    Update CompareToExample.java

--- Javashortproject/src/CompareToExample.java
+++ Javashortproject/src/CompareToExample.java
@@ -6,10 +6,12 @@ public class CompareToExample {
 		String s3 = "meklo";
 		String s4 = "Heflo";
 		String s5 = "jello";
+		String s6 = "heyyo";
 		System.out.println(s1.compareTo(s2));
 		System.out.println(s1.compareTo(s3));
 		System.out.println(s1.compareTo(s4));
 		System.out.println(s2.compareTo(s5));
+		System.out.println(s2.compareTo(s6));
 		
 	
 	}
