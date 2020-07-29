5400a22aaf7381500f955fec1f1d4d8408a2a359
snehagautam5151 <interio.sneha@gmail.com>
2020-07-29 14:34:10 +0530

    practice 29 jully

---
+++ Javashortproject/src/CompareToExample.java
@@ -0,0 +1,17 @@
+
+public class CompareToExample {
+	public static void main(String agrs[]) {
+		String s1 = "Hello";
+		String s2 = "Hello";
+		String s3 = "meklo";
+		String s4 = "Heflo";
+		String s5 = "jello";
+		System.out.println(s1.compareTo(s2));
+		System.out.println(s1.compareTo(s3));
+		System.out.println(s1.compareTo(s4));
+		System.out.println(s2.compareTo(s5));
+		
+	
+	}
+
+}
---
+++ Javashortproject/src/ConcatExample.java
@@ -0,0 +1,13 @@
+
+public class ConcatExample {
+	public static void main(String agrs[]) {
+		String s1 = "java strings";
+		s1.concat("sample");
+		System.out.println(s1);
+		s1 = s1.concat("sample exapmle or concat");
+		System.out.println(s1);
+		
+			
+	}
+
+}
